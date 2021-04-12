daef7ddca5dad81cb0d02f716f73dd70d2cc1416
lucascapl <71772605+lucascapl@users.noreply.github.com>
2021-04-12 00:32:05 -0300

    Create paint.java

---
+++ Projeto 1 de 2 Semana 1/Apenas Trecho das funcionalidades/paint.java
@@ -0,0 +1,25 @@
+//A parte nova é a partir da linha 11, onde desenho retangulo ao redor da figura focada.
+public void paint(Graphics g)
+    {
+        super.paint(g);
+        Graphics2D gfd = (Graphics2D) g;
+        for (Figure fig: this.figs)
+        {
+            fig.paint(g);
+        }
+        
+        if (focus != null)
+        {
+            if (focus.getClass().equals(Texto.class))  //Focar o texto
+            {
+                gfd.setColor(Color.red);
+                gfd.drawRect(focus.x-2, focus.y - 18, focus.w+5, 25);
+            }
+            else  //Focar demais figuras
+            {
+                gfd.setColor(Color.RED);
+                gfd.drawRect(focus.x-2, focus.y-2, focus.w+4, focus.h+4);
+            }
+        }
+        focus.paint(g);  //Z-order
+    }
