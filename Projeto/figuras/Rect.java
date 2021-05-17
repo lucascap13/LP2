3f8c05b59d6709b8a3a632ee373cdf80e17fd78c
lucascapl <71772605+lucascapl@users.noreply.github.com>
2021-05-16 22:17:09 -0300

    Update Rect.java

--- Projeto/figuras/Rect.java
+++ Projeto/figuras/Rect.java
@@ -19,4 +19,26 @@ public class Rect extends Figure
         g2d.setColor(new Color(this.r, this.g, this.b));
         g2d.drawRect(this.x,this.y, this.w,this.h);
     }
+    
+    public boolean clicked(int posX, int posY)
+    {
+        if((posX >= this.x && posX <= (this.w+this.x)) && (posY >= this.y && posY <= (this.y+this.h)))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void Focar (Graphics g)
+    {
+        Graphics2D gfd = (Graphics2D) g;
+        gfd.setColor(Color.RED);
+        gfd.drawRect(this.x-2, this.y-2, this.w+4, this.h+4);
+        gfd.setColor(Color.CYAN);
+        gfd.fillOval(this.x+this.w-4,this.y-3,7,7);
+        gfd.fillOval(this.x-3,this.y+this.h-6,7,7);
+    }
 }
