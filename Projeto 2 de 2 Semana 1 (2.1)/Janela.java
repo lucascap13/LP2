16b720687d8fba9e0e81c2bf6eae4ca12a60c161
lucascapl <71772605+lucascapl@users.noreply.github.com>
2021-05-16 21:41:00 -0300

    Create Janela.java

---
+++ Projeto 2 de 2 Semana 1 (2.1)/Janela.java
@@ -0,0 +1,9 @@
+public class Janela extends JPanel
+{
+  ...//Mantive o mesmo formato da antiga Frame, apenas adicionei essas partes que especifiquei aqui
+  Janela()
+  {
+        setFocusable(true);
+        setDoubleBuffered(true);
+        ...
+   }
