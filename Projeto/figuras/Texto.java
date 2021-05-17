d62d8f12046cf4e87d3f5d0a22b59d057b850747
lucascapl <71772605+lucascapl@users.noreply.github.com>
2021-05-16 22:17:53 -0300

    Update Texto.java

--- Projeto/figuras/Texto.java
+++ Projeto/figuras/Texto.java
@@ -26,4 +26,23 @@ public class Texto extends Figure
         this.h= metrics.getHeight();
         this.w = metrics.stringWidth(cadeia);
     }
+    
+    public boolean clicked(int posX, int posY)
+    {
+        if((posX >= this.x && posX <= (this.w+this.x)) && (posY <= this.y && posY >= (this.y-this.h)))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void Focar (Graphics g)
+    {
+        Graphics2D gfd = (Graphics2D) g;
+        gfd.setColor(Color.red);
+        gfd.drawRect(this.x-2, this.y - 18, this.w+5, 25);
+    }
 }
