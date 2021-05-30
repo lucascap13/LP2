27ff5d1bdd06ce0d716c916140f89bb3b4c364ae
lucascapl <71772605+lucascapl@users.noreply.github.com>
2021-05-30 11:25:01 -0300

    Create ArrayList_botoes_foco.java

---
+++ Projeto 2 de 2 Semana 3 (2.3)/ArrayList_botoes_foco.java
@@ -0,0 +1,7 @@
+public class Janela extends JPanel
+{
+    ... 
+    ArrayList<Botao> botoes = new ArrayList<Botao>();                                                                 //Lista de botoes
+    Botao focusB, focusTinta = null;                                                                                  //Selecao de botao
+    ...
+}
