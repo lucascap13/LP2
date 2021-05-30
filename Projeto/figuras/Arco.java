fdc2e63a6103b71948e26607b7698c5ee2bf1e6f
lucascapl <71772605+lucascapl@users.noreply.github.com>
2021-05-30 11:41:13 -0300

    Update Arco.java

--- Projeto/figuras/Arco.java
+++ Projeto/figuras/Arco.java
@@ -3,39 +3,29 @@ import java.awt.*;
 
 public class Arco extends Figure
 {
-    public Arco (int x, int y, int w, int h, int r, int g, int b)
+    public Arco (int x, int y, int w, int h, int r, int g, int b, boolean focado)
     {
-        super(x, y, r, g, b, w, h);
+        super(x, y, r, g, b, w, h, focado);
         this.w = w;
         this.h = h;
     }
     
-    public void paint (Graphics g)
+    public void paint (Graphics g, boolean focado)
     {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(new Color(this.r, this.g, this.b));
-        g2d.drawArc(this.x, this.y, this.w, this.h, 90, 180);
-    }
-    
-    public boolean clicked(int posX, int posY)
-    {
-        if((posX >= this.x && posX <= (this.w+this.x)) && (posY >= this.y && posY <= (this.y+this.h)))
+        if(focado)
         {
-            return true;
+            g2d.setColor(new Color(this.r, this.g, this.b));
+            g2d.drawArc(this.x, this.y, this.w, this.h, 90, 180);
+            g2d.setColor(Color.RED);
+            g2d.drawRect(this.x-2, this.y-2, this.w+4, this.h+4);
+            this.focar(g);
         }
         else
         {
-            return false;
+            g2d.setColor(new Color(this.r, this.g, this.b));
+            g2d.drawArc(this.x, this.y, this.w, this.h, 90, 180);
         }
-    }
 
-    public void Focar (Graphics g)
-    {
-        Graphics2D gfd = (Graphics2D) g;
-        gfd.setColor(Color.RED);
-        gfd.drawRect(this.x-2, this.y-2, this.w+4, this.h+4);
-        gfd.setColor(Color.CYAN);
-        gfd.fillOval(this.x+this.w-4,this.y-3,7,7);
-        gfd.fillOval(this.x-3,this.y+this.h-6,7,7);
     }
 }
